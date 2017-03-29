aa8539f4c2bcf564532218889edda0d66f566d36
LeoNiedermeier <LeoNiedermeier@users.noreply.github.com>
2017-03-29 18:09:09 +0200

    delay overlay

--- src/app/navigation-loading-overlay/navigation-loading-overlay.component.ts
+++ src/app/navigation-loading-overlay/navigation-loading-overlay.component.ts
@@ -3,7 +3,7 @@ import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, Naviga
 
 @Component({
   selector: 'poa-navigation-loading-overlay',
-  // inline template with multiline string
+  // inline template with multiline string; backticks
   // Spinner in overlay with bootstrap 4 and font awesome
   // the background: rgba(0,0,0,0.5); prohibits inheritance of opacity to spinner
   template: `<div *ngIf="loading" class="modal-content modal-backdrop justify-content-center align-items-center w-100 h-100" 
@@ -14,8 +14,9 @@ import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, Naviga
 })
 export class NavigationLoadingOverlayComponent {
 
-  // Sets initial value to true to show loading spinner on first load
-  loading = true;
+  loading = false;
+
+  private counter = 0;
 
   constructor(router: Router) {
     router.events.subscribe((event: Event) => {
@@ -23,16 +24,31 @@ export class NavigationLoadingOverlayComponent {
     });
   }
 
-
   // Shows and hides the loading spinner during RouterEvent changes
   navigationInterceptor(event: Event): void {
     // http://stackoverflow.com/questions/37069609/show-loading-screen-when-navigating-between-routes-in-angular-2
     if (event instanceof NavigationStart) {
-      this.loading = true;
+      // the counter is an indicator that we are between start and stop loading (counter > 0)
+      this.counter++;
+      // the loading should be set with a small time delay
+      setTimeout(() => {
+        // if counter === 0, there was a NavigationEnd event (etc) meanwhile -> do not show overlay
+        if (this.counter > 0 && !this.loading) {
+          this.loading = true;
+        }
+      }, 100);
+
+      // Note: use "=>": it is a function expression! Not direct use of the checkAndSetLoading method.
+      // setTimeout(() => this.checkAndSetLoading(), 100);
     }
     // do not forget cancel and error case!
     if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
-      this.loading = false;
+      if (this.counter > 0) {
+        this.counter--;
+      }
+      if (this.counter === 0 && this.loading) {
+        this.loading = false;
+      }
     }
     // the RoutesRecognized event does nothing
   }
