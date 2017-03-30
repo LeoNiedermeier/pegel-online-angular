364d37eb84cd6ed22744574e86f164bda56998d3
LeoNiedermeier <LeoNiedermeier@users.noreply.github.com>
2017-03-30 18:43:50 +0200

    paging

--- src/app/poa/waters-table/waters-table.component.ts
+++ src/app/poa/waters-table/waters-table.component.ts
@@ -10,11 +10,52 @@ import { ActivatedRoute } from '@angular/router';
 export class WatersTableComponent implements OnInit {
   waters: Water[] = [];
 
+  private linesPerPage = 10;
+
+  currentPage = 0;
+
+  private data: Water[] = [];
+
   constructor(private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
-    this.route.data.subscribe((data: { waters: Water[] }) => this.waters = data.waters);
+    this.route.data.subscribe((data: { waters: Water[] }) => { this.data = data.waters; this.currentPage = 0; this.recalculatePages(); });
+  }
+
+  public nextPage(): void {
+    if (this.hasNextPage()) {
+      this.currentPage++;
+      this.recalculatePages();
+    }
+  }
+
+  public previousPage(): void {
+    if (this.hasPreviousPage) {
+      this.currentPage--;
+      this.recalculatePages();
+    }
+  }
+
+
+
+  private recalculatePages() {
+    this.waters = this.data.slice(this.currentPage * this.linesPerPage, (this.currentPage + 1) * this.linesPerPage);
+  }
+
+  hasNextPage(): boolean {
+    return (this.currentPage + 1) * this.linesPerPage < this.data.length;
+  }
+
+  hasPreviousPage(): boolean {
+    return this.currentPage > 0;
+  }
+
+  gotoPage(page: number): void {
+    if (page > 0 && (page + 1) * this.linesPerPage < this.data.length) {
+      this.currentPage = page;
+      this.recalculatePages();
+    }
   }
 }
